af366be74db084459b42ada43a697d26fdc21bc0
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2020-01-22 16:17:45 +0100

    Controlador Usuarios Creado

---
+++ Controllers/USUARIOSController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Astrofilm.Models;
+
+namespace Astrofilm.Controllers
+{
+    public class USUARIOSController : Controller
+    {
+        private AstrofilmEntities db = new AstrofilmEntities();
+
+        // GET: USUARIOS
+        public ActionResult Index()
+        {
+            var uSUARIOS = db.USUARIOS.Include(u => u.USUARIOS_AMIGOS);
+            return View(uSUARIOS.ToList());
+        }
+
+        // GET: USUARIOS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDUsuario = new SelectList(db.USUARIOS_AMIGOS, "IDAmigoFK", "IDAmigoFK");
+            return View();
+        }
+
+        // POST: USUARIOS/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDUsuario,Apellidos,Nombre,FecNac,TipoUser,Email,Premium")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIOS.Add(uSUARIOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDUsuario = new SelectList(db.USUARIOS_AMIGOS, "IDAmigoFK", "IDAmigoFK", uSUARIOS.IDUsuario);
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDUsuario = new SelectList(db.USUARIOS_AMIGOS, "IDAmigoFK", "IDAmigoFK", uSUARIOS.IDUsuario);
+            return View(uSUARIOS);
+        }
+
+        // POST: USUARIOS/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDUsuario,Apellidos,Nombre,FecNac,TipoUser,Email,Premium")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDUsuario = new SelectList(db.USUARIOS_AMIGOS, "IDAmigoFK", "IDAmigoFK", uSUARIOS.IDUsuario);
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // POST: USUARIOS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            db.USUARIOS.Remove(uSUARIOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
