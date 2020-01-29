8fede4312d42b100ba8b8b2dbc5451acdafe084f
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2020-01-29 16:58:00 +0100

    haciendo vinculacion al crear usuario con BD Roles

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Astrofilm.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,19 @@ namespace Astrofilm.Controllers
     {
         public ActionResult Index()
         {
+            //DefaultConnection db = new DefaultConnection();
+            AstrofilmEntities db = new AstrofilmEntities();
+
+            // Si existe el empleado correspondiente al usuario actual
+            // se va a View, en caso contrario se va a crear el empleado.
+            string usuario = User.Identity.GetUserName();
+            var empleado = db.USUARIOS.Where(e => e.Email == usuario).FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            empleado == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
             return View();
         }
 
---
+++ Controllers/MisDatosController.cs
@@ -0,0 +1,44 @@
+﻿using Astrofilm.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Astrofilm.Controllers
+{
+    [Authorize(Roles = "Usuario")]
+    public class MisDatosController : Controller
+    {
+        AstrofilmEntities db = new AstrofilmEntities();
+        // GET: MisDatos
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: MisDatos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: MisDatos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include =
+ "Id,Nombre,Email,Telefono,FechaNacimiento")] USUARIOS usuario)
+        {
+            // Para asignar el Nombre del usuario identificado al campo Email de Empleado
+            usuario.Email = User.Identity.GetUserName();
+            if (ModelState.IsValid)
+            {
+                db.USUARIOS.Add(usuario);
+                db.SaveChanges();
+                // Redirige a la acción Index del controlador Home
+                return RedirectToAction("Index", "Home");
+            }
+            return View(usuario);
+        }
+
+    }
+}
