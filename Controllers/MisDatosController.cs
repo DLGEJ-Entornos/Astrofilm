7a330ae0e3fcbcb2f5571be2e10abea6667225d9
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2020-01-30 17:48:01 +0100

    Haciendo edicion de datos de usuario, error con ocultar id en vista , error en controlador al guardar datos al comentar id en vista

--- Controllers/MisDatosController.cs
+++ Controllers/MisDatosController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,10 +27,11 @@ namespace Astrofilm.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include =
-         "Id,Nombre,Apellidos,FecNac,TipoUser,Premium")] USUARIOS usuario)
+         "Id,Nombre,Apellidos,FecNac,TipoUser")] USUARIOS usuario)
         {
-            // Para asignar el Nombre del usuario identificado al campo Email de Empleado
+            // Para asignar el Nombre del usuario identificado al campo Email de  Usuario
             usuario.Email = User.Identity.GetUserName();
+            //usuario.Premium = false;
             if (ModelState.IsValid)
             {
                 db.USUARIOS.Add(usuario);
@@ -39,6 +41,35 @@ namespace Astrofilm.Controllers
             }
             return View(usuario);
         }
-
+        // GET: MisDatos/Edit
+        public ActionResult Edit()
+        {
+            // Se seleccionan los datos del empleado correspondiente al usuario actual
+            string wUsuario = User.Identity.GetUserName();
+            var usuario = db.USUARIOS.Where(e => e.Email == wUsuario).FirstOrDefault();
+            if (usuario == null)
+            {
+                // Si no existe el empleado, redirige a la acción Index del controlador Home
+                return RedirectToAction("Index", "Home");
+            }
+            // Si existe el empleado correspondiente se va a View
+            return View(usuario);
+        }
+        // POST: MisDatos/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //"Id,Nombre,Apellidos,FecNac,TipoUser"
+        public ActionResult Edit([Bind(Include =
+         "Id,Nombre,Email,Apellidos,TipoUser,Premium,FecNac")] USUARIOS usuario)
+        {
+            usuario.Email = User.Identity.GetUserName();
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(usuario);
+        }
     }
 }
