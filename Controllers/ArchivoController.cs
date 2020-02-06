35b638d7786fa8019aa5eec8b58f5aa41d0a59b9
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2020-02-06 01:23:40 +0100

    haciendo el metodo get del controlador archivo

--- Controllers/ArchivoController.cs
+++ Controllers/ArchivoController.cs
@@ -8,124 +8,27 @@ namespace Astrofilm.Controllers
 {
     public class ArchivoController : Controller
     {
-        private AstrofilmEntities astrofilmEntities = new AstrofilmEntities();
+        private AstrofilmEntities db = new AstrofilmEntities();
 
-        ////////////////////////SUBSTITUYE POR ARCHIVO
-        // GET: CRITICAS
-        [Authorize(Roles = "Administrador")]
+        // GET: USUARIOS
+        //[Authorize(Roles = "Administrador")] //Quiero que acceda cualquiera
         public ActionResult Index()
         {
-            var cRITICAS = db.CRITICAS.Include(c => c.PELICULAS).Include(c => c.USUARIOS);
-            return View(cRITICAS.ToList());
-        }
-
-        // GET: CRITICAS/Details/5
-        [Authorize(Roles = "Administrador")]
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            //var uSUARIOS = db.USUARIOS.Include(u => u.USUARIOS_AMIGOS);
+            var ArchivoTablas = new ArchivoModels
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CRITICAS cRITICAS = db.CRITICAS.Find(id);
-            if (cRITICAS == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cRITICAS);
+                IDPelicula = db.IDPelicula.toList(),
+                Titulo = db.Titulo.toList(),
+                IDTrabajador = db.IDTrabajador.toList(),
+                Tipo = db.Tipo.toList(),
+                IDGenero = db.IDGenero.toList(),
+                Nombre = db.Nombre.toList(),
+                IDLista = db.IDLista.toList(),
+                Titutlo = db.Titutlo.toList()
+
+            };
+            return View(ArchivoTablas);
         }
-
-        // GET: CRITICAS/Create
-        [Authorize(Roles = "Administrador")]
-        public ActionResult Create()
-        {
-            ViewBag.IDPeliFK = new SelectList(db.PELICULAS, "IDPelicula", "Titulo");
-            ViewBag.IDUserFK = new SelectList(db.USUARIOS, "IDUsuario", "Apellidos");
-            return View();
-        }
-
-        // POST: CRITICAS/Create
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IDCritica,Texto,IDUserFK,IDPeliFK")] CRITICAS cRITICAS)
-        {
-            if (ModelState.IsValid)
-            {
-                db.CRITICAS.Add(cRITICAS);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.IDPeliFK = new SelectList(db.PELICULAS, "IDPelicula", "Titulo", cRITICAS.IDPeliFK);
-            ViewBag.IDUserFK = new SelectList(db.USUARIOS, "IDUsuario", "Apellidos", cRITICAS.IDUserFK);
-            return View(cRITICAS);
-        }
-
-        // GET: CRITICAS/Edit/5
-        [Authorize(Roles = "Administrador")]
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CRITICAS cRITICAS = db.CRITICAS.Find(id);
-            if (cRITICAS == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.IDPeliFK = new SelectList(db.PELICULAS, "IDPelicula", "Titulo", cRITICAS.IDPeliFK);
-            ViewBag.IDUserFK = new SelectList(db.USUARIOS, "IDUsuario", "Apellidos", cRITICAS.IDUserFK);
-            return View(cRITICAS);
-        }
-
-        // POST: CRITICAS/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IDCritica,Texto,IDUserFK,IDPeliFK")] CRITICAS cRITICAS)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(cRITICAS).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.IDPeliFK = new SelectList(db.PELICULAS, "IDPelicula", "Titulo", cRITICAS.IDPeliFK);
-            ViewBag.IDUserFK = new SelectList(db.USUARIOS, "IDUsuario", "Apellidos", cRITICAS.IDUserFK);
-            return View(cRITICAS);
-        }
-
-        // GET: CRITICAS/Delete/5
-        [Authorize(Roles = "Administrador")]
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CRITICAS cRITICAS = db.CRITICAS.Find(id);
-            if (cRITICAS == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cRITICAS);
-        }
-
-        // POST: CRITICAS/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            CRITICAS cRITICAS = db.CRITICAS.Find(id);
-            db.CRITICAS.Remove(cRITICAS);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
