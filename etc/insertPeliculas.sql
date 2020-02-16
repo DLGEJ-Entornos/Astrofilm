adcfd72d88f9ec90ee3b369152dda11221781aec
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2020-02-16 01:12:45 +0100

    Pre INSERT peliculas 2

---
+++ etc/insertPeliculas.sql
@@ -0,0 +1,201 @@
+USE [E:\SERVIDOR\SUPUESTOPRACTICOINTEGRADOR\ASTROFILM\APP_DATA\ASTROFILM.MDF]
+GO
+
+INSERT INTO [dbo].[PELICULAS]
+           ([Titulo]
+           ,[Año]
+           ,[PuntMedia]
+           ,[Imagen])
+     --VALUES
+     --      (<Titulo, nvarchar(50),>
+     --      ,<Año, date,>
+     --      ,<PuntMedia, decimal(2,1),>
+     --      ,<Imagen, nvarchar(50),>)
+	 VALUES
+           --('El Gran Lebowski'
+           --,'1998'
+           --,3
+           --,'The-Big-Lebowski.jpg');
+		   ('Relatos Salvajes'
+           ,'2014'
+           ,3
+           ,'relatosSalvajes.jpg'),
+		   ('Con Faldas y a lo Loco'
+           ,'1959'
+           ,3
+           ,'faldasAloLoco.jpg'),
+		   ('La Vida de Brian'
+           ,'1979'
+           ,3
+           ,'vidaBrian.jpg'),
+		   ('Parásitos'
+           ,'2019'
+           ,3
+           ,'parasite.jpg'),
+		   ('Días de Radio'
+           ,'1987'
+           ,3
+           ,'radioDays.jpg'),
+		   ('Captain Fantastic'
+           ,'2016'
+           ,3
+           ,'Captain_Fantastic.jpg'),
+		   ('Poderosa Afrodita'
+           ,'1995'
+           ,3
+           ,'mightyAphrodite.jpg'),
+		   ('Good bye, Lenin!'
+           ,'2003'
+           ,3
+           ,'goodByeLennin.jpg'),
+		   ('Babel'
+           ,'2006'
+           ,3
+           ,'Babel.jpg'),
+		   ('Amores Perros'
+           ,'2000'
+           ,3
+           ,'amoresPerros.jpg'),
+		   ('Mustang'
+           ,'2015'
+           ,3
+           ,'Mustang.jpg'),
+		   ('Las uvas de la ira'
+           ,'1940'
+           ,3
+           ,'uvasIra.jpg'),
+		   ('Mogambo'
+           ,'1953'
+           ,3
+           ,'mogambo.jpg'),
+		   ('Hierro 3'
+           ,'2004'
+           ,3
+           ,'hierro3.jpg'),
+		   ('La Chaqueta Metálica'
+           ,'1984'
+           ,3
+           ,'ChaquetaMetalica.jpg'),
+		   ('Lore'
+           ,'2012'
+           ,3
+           ,'LORE.jpg'),
+		   ('Dersu Uzala'
+           ,'1975'
+           ,3
+           ,'dersuUzala.jpg'),
+		   ('Million Dollar Baby'
+           ,'2004'
+           ,3
+           ,'Million_Dollar_Baby.jpg'),
+		   ('Trainspotting'
+           ,'1996'
+           ,3
+           ,'trainspotting.jpg'),
+		   ('Cowboy de Media Noche'
+           ,'1969'
+           ,3
+           ,'CowboyMedianoche.jpg'),
+		   ('Déjame Entrar'
+           ,'2008'
+           ,3
+           ,'dejameEntrar.jpg'),
+		   ('Rio Rojo'
+           ,'1948'
+           ,3
+           ,'redRiver.jpg'),
+		   ('Django Desencadenado'
+           ,'2012'
+           ,3
+           ,'djangoDesencadenado.jpg'),
+		   ('Comanchería'
+           ,'2016'
+           ,3
+           ,'Comancheria.jpg'),
+		   ('Sin Perdón'
+           ,'1992'
+           ,3
+           ,'sinPerdon.jpg'),
+		   ('Centauros del Desierto'
+           ,'1956'
+           ,3
+           ,'centaurosDelDesierto.jpg'),
+		   ('El Nombre de la Rosa'
+           ,'1986'
+           ,3
+           ,'Nombre-de-la-rosa.jpg'),
+		   ('Corre Lola Corre'
+           ,'1998'
+           ,3
+           ,'runLolaRun.jpg'),
+		   ('Fargo'
+           ,'1996'
+           ,3
+           ,'fargo.jpg'),
+		   ('Erase una vez en HollyHood'
+           ,'2019'
+           ,3
+           ,'eraseUnaVezHollywood.jpg'),
+		   ('Thelma & Louise'
+           ,'1991'
+           ,3
+           ,'thelmaLouise.jpg'),
+		   ('Midsommar'
+           ,'2019'
+           ,3
+           ,'midsommar.jpg'),
+		   ('La Pesadilla de Darwin'
+           ,'2004'
+           ,3
+           ,'pesadillaDarwin.jpg'),
+		   ('Searching for Sugar Man'
+           ,'2012'
+           ,3
+           ,'Searching_for_Sugar_Man.jpg'),
+		   ('Pervert guide to ideology'
+           ,'2012'
+           ,3
+           ,'PervertGuide.jpg'),
+		   ('Lets get lost'
+           ,'1998'
+           ,3
+           ,'letsGetLost.jpg'),
+		   ('Requiem for a Dream'
+           ,'2000'
+           ,3
+           ,'requiem_for_a_dream.jpg'),
+		   ('Una historia verdadera'
+           ,'1999'
+           ,3
+           ,'unaHistoriaVerdadera.jpg'),
+		   ('Into the wild'
+           ,'2007'
+           ,3
+           ,'intoTheWild.jpg'),
+		   ('¡Ólvidate de mí!'
+           ,'2004'
+           ,3
+           ,'olvidateDeMi.jpg'),
+		   ('The Man From Earth'
+           ,'2007'
+           ,3
+           ,'the_man_from_earth.jpg'),
+		   ('Donnie Darko'
+           ,'2001'
+           ,3
+           ,'donnieDarko.jpg'),
+		   ('Memento'
+           ,'2000'
+           ,3
+           ,'memento.jpg'),
+		   ('Coherence'
+           ,'2013'
+           ,3
+           ,'coherence.jpg'),
+		   ('They Live'
+           ,'1988'
+           ,3
+           ,'theyLive.jpg');
+GO
+
+
